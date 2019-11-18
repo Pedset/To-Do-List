55fc5115138d25a149cd38965404bf618d8601a3
Pedset <57757244+Pedset@users.noreply.github.com>
2019-11-18 09:42:32 +0100

    My first commit

---
+++ To-Do-List/toD.js
@@ -0,0 +1,76 @@
+
+function yellow() {
+    document.body.style.backgroundColor = "yellow";
+}
+function green() {
+    document.body.style.backgroundColor = "green";
+}
+function blue() {
+    document.body.style.backgroundColor = "blue";
+}
+
+
+
+$(document).ready(
+    function(){
+        $('#button').click(
+            function(){
+                if ($('input[name=textbox]').val() !== '') {
+                var toAdd = $('input[name=textbox]').val();
+                 $('ol').append('<li>' + toAdd + '</li>');}
+                $('input[name=textbox]').val("");
+            });
+       
+       $("input[name=textbox]").keyup(function(event1){
+          if(event1.keyCode == 13){
+             
+            $("#button").click();
+          }         
+      });
+      
+      $(document).on('click','li', function(){
+        $(this).toggleClass('strike');    
+      });
+      
+      $('input').focus(function() {
+        $(this).val('');
+      });
+      
+      $('#hs').sortable();  
+        
+        //
+        
+        $('#btn_settings').click(
+            function(){
+                $('.strike').hide();
+            });
+        
+      //
+        
+        $("#btn_color ,#btn_settings").mouseenter(function() {
+        $(this).animate({
+            width: "85px",
+            height: "40px"
+            
+        }, 800 );});
+        $("#btn_color, #btn_settings").mouseleave(function() {
+        $(this).animate({
+            width: "64px",
+            height: "22px"
+            
+        }, 1300 );});
+        //
+        
+  $("#btn_color").click(function(){
+    $("#slide").slideDown("slow");
+  });
+        //
+        
+        $("#slide").mouseleave(function() {
+        $("#slide").slideUp("slow");
+  });
+        
+        //
+        
+    }
+);
